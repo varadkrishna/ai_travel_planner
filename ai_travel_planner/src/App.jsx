95034d194d785dd749aa4a4666260a79d4f0901c
sara-khan26 <srkh725@gmail.com>
2025-04-19 14:48:33 +0530

    modified:   src/App.jsx         deleted:    src/create-trip/index.jsx         modified:   src/main.jsx src/create-trip/CreateTrip.jsx

--- ai_travel_planner/src/App.jsx
+++ ai_travel_planner/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/custom/Header'
+import CreateTrip from './create-trip/CreateTrip'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header/>
      <h1>Hello</h1>
+    <CreateTrip/>
     </>
   )
 }
--- ai_travel_planner/src/main.jsx
+++ ai_travel_planner/src/main.jsx
@@ -4,18 +4,23 @@ import App from './App'
 import './index.css'  // Add this line
 import { createBrowserRouter } from 'react-router-dom'
 import { RouterProvider } from 'react-router'
+import Header from './components/custom/Header'
+import CreateTrip from './create-trip/CreateTrip'
 
 const router=createBrowserRouter([
   {
     path: '/',
     element: <App />
     },
-    
+  
+    {
+      path: '/createtrip',
+      element: <CreateTrip />
+      },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header/>
     <RouterProvider router={router} />
   </React.StrictMode>
 )
